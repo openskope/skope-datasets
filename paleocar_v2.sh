0afe8cdca3210ae72d26bc709ca73f3e537e76f8
Kyle Bocinsky <bocinsky@gmail.com>
2021-12-01 08:31:40 -0700

    maize_farming_niche

--- paleocar_v2.sh
+++ paleocar_v2.sh
@@ -54,36 +54,32 @@ gdal_translate -ot Int16 -co BIGTIFF=YES -co TILED=YES -co BLOCKXSIZE=16 -co BLO
 )
 
 
-## maize_farming_niche
-#mkdir -p paleocar_v2/maize_farming_niche/geoserver
-#
-#gdal_translate -ot Int16 -co BIGTIFF=YES -co TILED=NO --config GDAL_PAM_ENABLED NO paleocar_v2/ppt_water_year/cube.tif paleocar_v2/maize_farming_niche/ppt.tif
-#gdal_translate -ot Int16 -co BIGTIFF=YES -co TILED=NO --config GDAL_PAM_ENABLED NO paleocar_v2/gdd_may_sept/cube.tif paleocar_v2/maize_farming_niche/gdd.tif
-#
-#gdal_calc.py -A paleocar_v2/maize_farming_niche/ppt.tif -B paleocar_v2/maize_farming_niche/gdd.tif --allBands=A --allBands=B --outfile=paleocar_v2/maize_farming_niche/niche.tif --calc="(A>=300)*(B>=1800)" --type=Byte --creation-option BIGTIFF=YES --creation-option TILED=NO --overwrite
-#
-#gdal_translate -ot Byte -co BIGTIFF=YES -co TILED=YES -co BLOCKXSIZE=16 -co BLOCKYSIZE=16 -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS --config GDAL_PAM_ENABLED NO paleocar_v2/maize_farming_niche/niche.tif paleocar_v2/maize_farming_niche/cube.tif
-#
-#rm paleocar_v2/maize_farming_niche/ppt.tif
-#rm paleocar_v2/maize_farming_niche/gdd.tif
-#rm paleocar_v2/maize_farming_niche/niche.tif
-#
-#(
-#  for year in {0001..2000}; do
-#    ((i=i%N)); ((i++==0)) && wait
-#    # echo "paleocar_v2/maize_farming_niche/geoserver/paleocar_v2_maize_farming_niche_$year.tif"
-#    if [ -f "paleocar_v2/maize_farming_niche/geoserver/paleocar_v2_maize_farming_niche_$year.tif" ]
-#    then
-#      continue
-#    else
-#      let band=$((10#$year)); gdal_translate paleocar_v2/maize_farming_niche/cube.tif paleocar_v2/maize_farming_niche/geoserver/paleocar_v2_maize_farming_niche_$year.tif -b $band -of COG -ot Byte -co BLOCKSIZE=128 -co OVERVIEWS=NONE -co COMPRESS=DEFLATE -q &
-#    fi
-#  done
-#)
+# maize_farming_niche
+mkdir -p paleocar_v2/maize_farming_niche/geoserver
+
+(
+  for year in {0001..2000}; do
+    ((i=i%N)); ((i++==0)) && wait
+    # echo "paleocar_v2/maize_farming_niche/geoserver/paleocar_v2_maize_farming_niche_$year.tif"
+    if [ -f "paleocar_v2/maize_farming_niche/geoserver/paleocar_v2_maize_farming_niche_$year.tif" ]
+    then
+      continue
+    else
+      let band=$((10#$year)); gdal_calc.py -A paleocar_v2/ppt_water_year/geoserver/paleocar_v2_ppt_water_year_$year.tif -B paleocar_v2/gdd_may_sept/geoserver/paleocar_v2_gdd_may_sept_$year.tif --outfile=paleocar_v2/maize_farming_niche/geoserver/temp.tif --calc="(A>=300)*(B>=1800)" --type=Byte --co TILED=YES --co BLOCKXSIZE=128 --co BLOCKYSIZE=128 --co COMPRESS=DEFLATE --overwrite --quiet; gdal_translate paleocar_v2/maize_farming_niche/geoserver/temp.tif paleocar_v2/maize_farming_niche/geoserver/paleocar_v2_maize_farming_niche_$year.tif -of COG -ot Byte -co BLOCKSIZE=128 -co OVERVIEWS=NONE -co COMPRESS=DEFLATE -q &
+    fi
+  done
+)
+rm paleocar_v2/maize_farming_niche/geoserver/temp.tif
+
+
 
 rsync -razhv paleocar_v2/ppt_water_year skope_staging:/projects/skope/datasets/paleocar_v2/
 rsync -razhv paleocar_v2/gdd_may_sept skope_staging:/projects/skope/datasets/paleocar_v2/
-# rsync -razhv paleocar_v2/maize_farming_niche skope_staging:/projects/skope/datasets/paleocar_v2/
+
+gdalbuildvrt -separate paleocar_v2/maize_farming_niche/cube.vrt paleocar_v2/maize_farming_niche/geoserver/*.tif
+gdal_translate -ot Byte -co BIGTIFF=YES -co TILED=YES -co BLOCKXSIZE=16 -co BLOCKYSIZE=16 -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS --config GDAL_PAM_ENABLED NO paleocar_v2/maize_farming_niche/cube.vrt paleocar_v2/maize_farming_niche/cube.tif
+
+rsync -razhv paleocar_v2/maize_farming_niche skope_staging:/projects/skope/datasets/paleocar_v2/
 
 #python main.py geoserver load --host https://geoserver.openskope.org --workspace SKOPE --geoserver-base-path /projects/skope/datasets/paleocar_v2/ppt_water_year/geoserver --base-path /projects/skope/datasets/paleocar_v2/ppt_water_year/geoserver
 #python main.py geoserver load --host https://geoserver.openskope.org --workspace SKOPE --geoserver-base-path /projects/skope/datasets/paleocar_v2/gdd_may_sept/geoserver --base-path /projects/skope/datasets/paleocar_v2/gdd_may_sept/geoserver
